fe70f49b77b3a96a83548da667a25fcf04dea7fe
KasiaWojtowicz <kwoj27@yahoo.ie>
2017-04-23 14:56:48 +0200

    added connection

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+
+
+//Stwórz poniżej odpowiednie zmienne z danymi do bazy
+$user = 'root';
+$password = 'coderslab';
+$dbname = 'Twitter_K'; //zmienić po utworzeniu bazy danych na jej nazwe
+$hostname = 'localhost';
