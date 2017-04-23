d518e54734034d2b1d027eac3dc077ca421b5ba3
KasiaWojtowicz <kwoj27@yahoo.ie>
2017-04-23 15:32:16 +0200

    added sql users readme

--- config/db.php
+++ config/db.php
@@ -5,5 +5,5 @@
 //Stwórz poniżej odpowiednie zmienne z danymi do bazy
 $user = 'root';
 $password = 'coderslab';
-$dbname = 'Twitter_K'; //zmienić po utworzeniu bazy danych na jej nazwe
+$dbname = 'Users'; //zmienić po utworzeniu bazy danych na jej nazwę
 $hostname = 'localhost';
